13d9574026afc8a7ad15ee73aa724e8bf9a9daa6
SemenBaranov <116193223+SemenBaranov@users.noreply.github.com>
2023-08-01 21:27:10 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 #Задача 22
-#from random import randint
-#n_set = set(randint(1, 20) for i in range(int(input('Введите кол-во элементов первого множества: '))))
-#print(n_set)
-#m_set = set(randint(1, 20) for i in range(int(input('Введите кол-во элементов второго множества: '))))
-#print(m_set)
-#s_set = sorted(n_set.intersection(m_set))
-#print(*s_set)11
+from random import randint
+n_set = set(randint(1, 20) for i in range(int(input('Введите кол-во элементов первого множества: '))))
+print(n_set)
+m_set = set(randint(1, 20) for i in range(int(input('Введите кол-во элементов второго множества: '))))
+print(m_set)
+s_set = sorted(n_set.intersection(m_set))
+print(*s_set)11
 
 #Задача 24
 from random import randint
